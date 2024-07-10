66dcaa0bd07af2a32ab9ad0198c17842ded7991c
yugecin <yugecin@users.noreply.github.com>
2024-07-10 20:02:56 +0200

    make explosion really explosive

--- explosion.c
+++ explosion.c
@@ -6,6 +6,9 @@ struct explosion {
 };
 struct explosion explosiontracker[GRID_CELLS_HORZ * GRID_CELLS_VERT];
 
+RECT explBounds;
+POINT explSize;
+
 void explosion_init()
 {
 	struct explosion *et;
@@ -31,12 +34,22 @@ void explosion_init()
 			}
 		}
 	}
+	explBounds.top = metrics.rcWork.top;
+	explBounds.right = metrics.rcWork.right - grid.size.x;
+	explBounds.bottom = metrics.rcWork.bottom - grid.size.y;
+	explBounds.left = metrics.rcWork.left;
+	explSize.x = explBounds.right - explBounds.left;
+	explSize.y = explBounds.bottom - explBounds.top;
 	for (i = 0; i < GRID_CELLS; i++) {
 		et = explosiontracker + i;
 		et->from.x = grid.pos.x + grid.size.x * (et->from.cellId % GRID_CELLS_HORZ);
 		et->from.y = grid.pos.y + grid.size.y * (et->from.cellId / GRID_CELLS_HORZ);
 		et->to.x = grid.pos.x + grid.size.x * (et->to.cellId % GRID_CELLS_HORZ);
 		et->to.y = grid.pos.y + grid.size.y * (et->to.cellId / GRID_CELLS_HORZ);
+		et->to.x += explSize.x * et->desktopX;
+		if (et->desktopX % 2) et->to.x -= et->to.x;
+		et->to.y += explSize.y * et->desktopY;
+		if (et->desktopY % 2) et->to.y -= et->to.y;
 	}
 }
 
@@ -44,10 +57,25 @@ void explosion_do(int index, float progress)
 {
 	struct explosion *et = explosiontracker + i;
 	struct win *win = wins.cells + i;
-	POINT pos, size;
+	POINT pos, size, isUneven;
 
 	size.x = size.y = 0;
 	pos.x = et->from.x + (int) ((et->to.x - et->from.x) * progress);
 	pos.y = et->from.y + (int) ((et->to.y - et->from.y) * progress);
+	// make origin 0,0
+	pos.x -= explBounds.left;
+	pos.y -= explBounds.top;
+	// check if desktop is uneven because then we need to mirror
+	isUneven.x = pos.x / explSize.x;
+	isUneven.y = pos.y / explSize.y;
+	// move back to working area
+	pos.x %= explSize.x;
+	pos.y %= explSize.y;
+	// mirror if uneven
+	if (isUneven.x) pos.x = explSize.x - pos.x;
+	if (isUneven.y) pos.y = explSize.y - pos.y;
+	// move back to desired origin
+	pos.x += explBounds.left;
+	pos.y += explBounds.top;
 	DemoSetWindowPos(win, pos, size, SWP_NOSIZE | SWP_NOACTIVATE);
 }
