cc3f25e3904bf99d29210021b6502bd64f069401
yugecin <yugecin@users.noreply.github.com>
2024-07-11 13:05:35 +0200

    give up on aspect ratio defined grid size
    can't get it working nicely and this is nice anyways because it kinda
    fills the screen

--- windowing.c
+++ windowing.c
@@ -16,20 +16,8 @@ struct {
 
 void grid_init()
 {
-	int w, h;
-
-	w = metrics.workingAreaWidth / (GRID_CELLS_HORZ + 3);
-	h = metrics.workingAreaHeight / (GRID_CELLS_VERT + 3);
-
-	if (w * (GRID_CELLS_HORZ + 3) * 9.0f / 16.0f > h * (GRID_CELLS_VERT + 3)) {
-		grid.size.x = h * 16 / 9;
-		grid.size.y = h;
-	} else {
-		grid.size.x = w;
-		grid.size.y = w * 9 / 16;
-	}
-	grid.size.x = w;
-	grid.size.y = h;
+	grid.size.x = metrics.workingAreaWidth / (GRID_CELLS_HORZ + 3);
+	grid.size.y = metrics.workingAreaHeight / (GRID_CELLS_VERT + 3);
 	grid.pos.x = metrics.rcWork.left + (metrics.workingAreaWidth - grid.size.x * GRID_CELLS_HORZ) / 2;
 	grid.pos.y = metrics.rcWork.top + (metrics.workingAreaHeight - grid.size.y * GRID_CELLS_VERT) / 2;
 }
